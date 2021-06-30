dd0c342d50b4b9716a2ac562e6f1891e71b25122
Alikhan <73962071@continental.edu.pe>
2021-06-30 18:33:38 -0500

    added model

---
+++ app/src/main/java/com/paparazziteam/retrofitget/model/ModelPosts.java
@@ -0,0 +1,44 @@
+package com.paparazziteam.retrofitget.model;
+
+public class ModelPosts {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    public ModelPosts() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
