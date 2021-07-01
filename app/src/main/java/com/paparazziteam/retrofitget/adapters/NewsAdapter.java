525434090772b11e1f0b70558188fbc3d608eea9
Alikhan <73962071@continental.edu.pe>
2021-07-01 08:16:24 -0500

    added adapter

---
+++ app/src/main/java/com/paparazziteam/retrofitget/adapters/NewsAdapter.java
@@ -0,0 +1,40 @@
+package com.paparazziteam.retrofitget.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.paparazziteam.retrofitget.model.News;
+
+import java.util.List;
+
+public class NewsAdapter  extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    List<News> mNewsModel;
+    Context context;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
